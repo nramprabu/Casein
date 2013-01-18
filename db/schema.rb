49ce66a49b4f40c6a7f49f25228cb035e696da82
Ramprabhu <nramprabu@gmail.com>
2013-01-18 14:18:12 +0530

    schema changes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130109095604) do
+ActiveRecord::Schema.define(:version => 20130118064857) do
 
   create_table "articles", :force => true do |t|
     t.string   "name"
@@ -88,7 +88,24 @@ ActiveRecord::Schema.define(:version => 20130109095604) do
     t.integer  "photo_file_size"
     t.datetime "photo_updated_at"
     t.string   "last_name"
+    t.boolean  "terms_of_service"
   end
+
+  create_table "delayed_jobs", :force => true do |t|
+    t.integer  "priority",   :default => 0
+    t.integer  "attempts",   :default => 0
+    t.text     "handler"
+    t.text     "last_error"
+    t.datetime "run_at"
+    t.datetime "locked_at"
+    t.datetime "failed_at"
+    t.string   "locked_by"
+    t.string   "queue"
+    t.datetime "created_at",                :null => false
+    t.datetime "updated_at",                :null => false
+  end
+
+  add_index "delayed_jobs", ["priority", "run_at"], :name => "delayed_jobs_priority"
 
   create_table "events", :force => true do |t|
     t.string   "name"
